af9b015c9c643e593825dbfeec152f319aa78b19
Kareem Ellis <kareemellis55@gmail.com>
2023-05-20 14:15:36 -0500

    Changed backend routes to use MongoDB

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
 require('dotenv').config()
 const express = require('express')
-const mongoose = require('mongoose')
 const cors = require('cors')
+
+const app = express()
 
 //Import Models
 const Note = require('./models/note')
-
-const app = express()
 
 app.use(express.json())
 app.use(express.static('build'))
 app.use(cors())
 
-
-// const note = new Note({
-//   content: 'HTML is Easy',
-//   important: true,
-// })
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
@@ -28,36 +22,28 @@ app.get('/api/notes', (request, response) => {
   })
 })
 
-// app.get('/api/notes/:id', (request, response) => {
-//   const id = request.params.id
-//   const note = notes.find(note => note.id == id)
+app.get('/api/notes/:id', (request, response) => {
+  Note.findById(request.params.id).then(note => {
+    response.json(note)
+  })
+})
 
-//   if (note) {
-//     response.json(note)
-//   } else {
-//     response.status(404).end()
-//   }
-// })
+app.post('/api/notes', (request, response) => {
+  const body = request.body
 
-// app.post('/api/notes', (request, response) => {
-//   const body = request.body
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
+  }
 
-//   if (!body.content) {
-//     return response.status(400).json({ 
-//       error: 'content missing' 
-//     })
-//   }
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
 
-//   const note = {
-//     content: body.content,
-//     important: body.important || false,
-//     id: generateId(),
-//   }
-
-//   notes = notes.concat(note)
-
-//   response.json(note)
-// })
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
+})
 
 // app.delete('/api/notes/:id', (request, response) => {
 //   const id = Number(request.params.id)
